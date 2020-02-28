670c4741eef2f599a0d20af0f4a78966c529ea3d
Chaoui Mohammed <chaoui.90@hotmail.es>
2020-02-28 13:18:54 +0100

    First commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,27 +3,42 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 1,
+        value : this.props.value,
         imageUrl: 'https://picsum.photos/200',
-        tags:[]
+        tags:['tag1','tag2','tag3']
     };
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags</p>;
-        return   <ul>
-                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                  </ul>;
+
+
+    handleIncrement=product=>{
+        console.log(product);
+        this.setState({value: this.state.value+1});
     }
+
     render() { 
-       
+        console.log('prop',this.props);
         return (
         <React.Fragment>
-            {this.state.tags.length ===0 && 'Please create new Tag' }
-                {this.renderTags()}
-
+            {this.props.children}
+            <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button
+             onClick={()=> this.handleIncrement({id:1})} 
+            className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                 {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+            </ul>
         </React.Fragment>
         );
     }
- 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {value: count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
 }
  
 export default Counter;

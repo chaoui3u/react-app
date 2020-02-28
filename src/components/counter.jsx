c53dfa4ac11eda346cb4cd26c618085b2405068e
Chaoui Mohammed <chaoui.90@hotmail.es>
2020-02-28 14:23:33 +0100

    deleting local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
-    state = {
-        value : this.props.value,
-        imageUrl: 'https://picsum.photos/200',
-        tags:['tag1','tag2','tag3']
-    };
+
 
 
     handleIncrement=product=>{
@@ -21,22 +17,21 @@ class Counter extends Component {
             {this.props.children}
             <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button
-             onClick={()=> this.handleIncrement({id:1})} 
+             onClick={()=> this.props.onIncrement(this.props.counter)} 
             className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                 {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
+            <button onClick={() =>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+
         </React.Fragment>
         );
     }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {value: count} = this.state;
+        const {value: count} = this.props.counter;
         return count === 0 ? 'Zero' : count;
     }
 }
